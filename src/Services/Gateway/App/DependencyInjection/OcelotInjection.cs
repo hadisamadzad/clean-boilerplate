884ba4bd1702de13cde105d5c75b6300e7f45252
Hadi Samadzad <h.samadzad@gmail.com>
2024-05-13 01:50:34 +0100

    Add init gateway changes

---
+++ src/Services/Gateway/App/DependencyInjection/OcelotInjection.cs
@@ -0,0 +1,16 @@
+﻿using Gateway.DelegatingHandlers;
+using Ocelot.DependencyInjection;
+
+namespace Gateway.DependencyInjection;
+
+public static class OcelotInjection
+{
+    public static IServiceCollection AddConfiguredOcelot(this IServiceCollection services)
+    {
+        services
+            .AddOcelot()
+            .AddDelegatingHandler<GlobalDelegatingHandler>(global: true);
+
+        return services;
+    }
+}
